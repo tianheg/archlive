338bfd0e4cbe8687976f1b6cc0b81049e2436069
tianheg <yidajiabei@163.com>
2022-09-04 15:09:09 +0800

    script: adjust

--- scripts/pacrep-script.sh
+++ scripts/pacrep-script.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # Watch packages info inside packages.x86_64
-file="packages.x86_64"
+file="$(dirname $(dirname $(realpath $0) ) )/packages.x86_64"
+rm $(dirname $(realpath $0))/pacrep-info.txt
 while read -r line; do
 echo -e "############
 $line
-$(pacman -Si $line | sed -n '4p')" >> pacrep-info.txt
+$(pacman -Si $line | sed -n '4p')" >> "$(dirname $(realpath $0))/pacrep-info.txt"
 done <$file
 
 ## Refers
