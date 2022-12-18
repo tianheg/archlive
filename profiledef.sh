2224dec92aed49d34b463a45177d40f45656f35a
tianheg <yidajiabei@gmail.com>
2022-12-18 08:59:29 +0800

    profiledef.sh: rm ia32 bootmodes, cancel files' permissions' setting

--- profiledef.sh
+++ profiledef.sh
@@ -10,8 +10,6 @@ install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr'
            'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp'
-           'uefi-ia32.grub.eltorito'
            'uefi-x64.grub.esp'
            'uefi-x64.grub.eltorito')
 arch="x86_64"
@@ -20,10 +18,5 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
